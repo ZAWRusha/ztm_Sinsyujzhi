4fa94c447ae92e2b1d4e3801c41db45f43897ce4
Malkov Dmitry <fly-agaric@bk.ru>
2020-08-24 02:01:30 +0700

    Changed files due to partial design adjustments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // topmenu fixed
 var h_hght = 162; // distance from the menu to the top of the screen (px)
-var h_mrg = 0;    // distance to fix the menu from the top of the screen (px)
+var h_mrg = 0; // distance to fix the menu from the top of the screen (px)
 
-$(function () {
+$(function() {
 
 	var elem = $('.nav'); // class of the topmenu
 	var top = $(this).scrollTop();
@@ -11,7 +11,7 @@ $(function () {
 		elem.css('top', h_mrg);
 	}
 
-	$(window).scroll(function () {
+	$(window).scroll(function() {
 		top = $(this).scrollTop();
 
 		if (top + h_mrg < h_hght) {
@@ -63,9 +63,9 @@ var mySwiperLB = new Swiper('.swiper-containerLB', {
 });
 
 // button scrollUp **************************************
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$(window).scroll(function () {
+	$(window).scroll(function() {
 		if ($(this).scrollTop() > 100) {
 			$('.scrollup').fadeIn();
 		} else {
@@ -73,15 +73,17 @@ $(document).ready(function () {
 		}
 	});
 
-	$('.scrollup').click(function () {
-		$("html, body").animate({ scrollTop: 0 }, 600);
+	$('.scrollup').click(function() {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 600);
 		return false;
 	});
 
 	// modal (popup) **************************************
 
 	//closed all modals
-	$('.modal-bg, .modal-close, .burger_b-close, .burger_b-bg').click(function (event) {
+	$('.modal-bg, .modal-close, .burger_b-close, .burger_b-bg').click(function(event) {
 		event.preventDefault()
 		$('.modal-bg').fadeOut()
 		$('.burger_b-bg').fadeOut()
@@ -94,28 +96,28 @@ $(document).ready(function () {
 	})
 
 	//listen 'item' modal and open on click
-	$('.modal-btn__item').click(function (event) {
+	$('.modal-btn__item').click(function(event) {
 		event.preventDefault()
 		$('.modal-bg').fadeIn()
 		$('.modal-item').fadeIn()
 	})
 
 	//listen 'call' modal and open on click
-	$('.modal-btn__call').click(function (event) {
+	$('.modal-btn__call').click(function(event) {
 		event.preventDefault()
 		$('.modal-bg').fadeIn()
 		$('.modal-call').fadeIn()
 	})
 
 	//listen 'sended' modal and open on click
-	$('.modal-btn__sended').click(function (event) {
+	$('.modal-btn__sended').click(function(event) {
 		event.preventDefault()
 		$('.modal-bg').fadeIn()
 		$('.modal-sended').fadeIn()
 	})
 
 	//listen 'menu-burger_b' modal and open on click
-	$('.menu-burger_b').click(function (event) {
+	$('.menu-burger_b').click(function(event) {
 		event.preventDefault()
 		$('.burger_b-bg').fadeIn()
 		$('.burger_b-modal').fadeIn()
@@ -131,8 +133,8 @@ $(document).ready(function () {
 	})
 });
 // categories-nav (vertical-nav) **************************************
-$(document).ready(function () {
-	$('.categories-nav span').click(function (e) {
+$(document).ready(function() {
+	$('.categories-nav span').click(function(e) {
 		e.preventDefault();
 		$(this).parent().parent().find('ul').slideToggle();
 		$(this).parent().parent().toggleClass('active');
